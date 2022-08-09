c83f0b148ddb805aa5408aa3b284f8d1b15a9616
chase <lcy5656@qq.com>
2022-08-09 12:27:17 +0800

    my first python rqt plugin

---
+++ src/ulaagui/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+package_name = 'ulaagui'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, ['plugin.xml']),
+        ('lib/' + package_name, ['resource/' + 'MyPlugin.ui']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='robot',
+    maintainer_email='lcy5656@qq.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'ulaagui = ulaagui.ulaagui:main'
+        ],
+    },
+)
